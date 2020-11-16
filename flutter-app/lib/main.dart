585a36a135dc8449ec2bc21fce0a8d727da72237
Howard Zhou <lovemew67@gmail.com>
2020-11-16 11:45:36 +0800

    task: add sample app

--- flutter-app/lib/main.dart
+++ flutter-app/lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show debugDefaultTargetPlatformOverride;
-import 'app/random.dart';
+// import 'package:flutter/foundation.dart' show debugDefaultTargetPlatformOverride;
+
+import 'app/sample.dart';
+// import 'app/random.dart';
+
+// final version
+void main() => runApp(new SampleApp());
+
+// ================
+// ================
+// ================
+// ================
 
 // mobile, web version
 // void main() => runApp(new RandomWordsApp());
 
 // desktop version
-void main() {
-  // https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
-  // debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-
-  runApp(new RandomWordsApp());
-}
+// void main() {
+//   // https://github.com/flutter/flutter/wiki/Desktop-shells#target-platform-override
+//   // debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+//   runApp(new RandomWordsApp());
+// }
