5514519ad46dff80812eccbd74c91d30e472d2ce
Howard Zhou <howardzhou@m800.com>
2021-11-19 17:54:52 +0800

    task: finish clean arch part

--- golang-sample/cmd/server.go
+++ golang-sample/cmd/server.go
@@ -8,7 +8,12 @@ import (
 	"strings"
 	"syscall"
 
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+
 	"github.com/lovemew67/public-misc/cornerstone"
+	"github.com/lovemew67/public-misc/golang-sample/controllerv1"
+	"github.com/lovemew67/public-misc/golang-sample/repositoryv1/sqlite"
+	"github.com/lovemew67/public-misc/golang-sample/servicev1"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -26,6 +31,8 @@ func NewAPIServerCmd() *cobra.Command {
 		Use:   "server",
 		Short: "server",
 		Run: func(cmd *cobra.Command, args []string) {
+			funcName := "apiServerCmd.Run"
+
 			// shut sonar
 			_ = cmd.Runnable()
 			_ = len(args)
@@ -42,6 +49,33 @@ func NewAPIServerCmd() *cobra.Command {
 			// log config before initializing
 			jsIndent, _ := json.MarshalIndent(viper.AllSettings(), "", "\t")
 			log.Printf("[%s] initializing server with config: %s", cornerstone.GetAppName(), (jsIndent))
+
+			// init repository
+			staffV1Repositorier, errRepository := sqlite.NewStaffV1SQLiteRepositorier(systemCtx)
+			if errRepository != nil {
+				cornerstone.Panicf(systemCtx, "[%s] failed to create staff v1 repositiory, err: %+v", funcName, errRepository)
+			}
+
+			// init service
+			staffV1Service, errService := servicev1.NewStaffV1Servicer(staffV1Repositorier)
+			if errService != nil {
+				cornerstone.Panicf(systemCtx, "[%s] failed to create staff v1 service, err: %+v", funcName, errService)
+			}
+
+			// init grpc server
+			grpcServer := controllerv1.InitGRPCServer(staffV1Service)
+			grpcCanceller := controllerv1.GRPCListenAndServe(systemCtx, grpcServer)
+			defer grpcCanceller()
+
+			// init http server
+			ginServer := controllerv1.InitGinServer(staffV1Service)
+			ginCanceller := controllerv1.HTTPListenAndServe(systemCtx, ginServer)
+			defer ginCanceller()
+
+			// init grpc gateway
+			grpcGateway := controllerv1.InitGRPCGateway(systemCtx)
+			grpcGatewayCanceller := controllerv1.GRPCGatewayListenAndServe(systemCtx, grpcGateway)
+			defer grpcGatewayCanceller()
 
 			// add graceful shutdown
 			signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
