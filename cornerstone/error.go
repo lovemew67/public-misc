a7b62d4701602b87b26852ef678b263f68a6fce8
Howard Zhou <lovemew67@gmail.com>
2021-11-28 21:33:05 +0800

    task: change code error to interface

--- cornerstone/error.go
+++ cornerstone/error.go
@@ -4,14 +4,22 @@ import (
 	"fmt"
 )
 
-type CodeError struct {
+type CodeError interface {
+	ErrorCode() int
+	SubErrorCode() int
+	ErrorMessage() string
+	Error() string
+	FullError() string
+}
+
+type CarrierCodeError struct {
 	errorCode    int
 	subErrorCode int
 	errorMessage string
 }
 
-func NewCodeError(ec, sec int, em string) (result CodeError) {
-	result = CodeError{
+func NewCarrierCodeError(ec, sec int, em string) (result CarrierCodeError) {
+	result = CarrierCodeError{
 		errorCode:    ec,
 		subErrorCode: sec,
 		errorMessage: em,
@@ -19,29 +27,34 @@ func NewCodeError(ec, sec int, em string) (result CodeError) {
 	return
 }
 
-func FromNativeError(err error) (result CodeError) {
-	result = CodeError{
+func FromNativeError(err error) (result CarrierCodeError) {
+	result = CarrierCodeError{
 		errorMessage: err.Error(),
 	}
 	return
 }
 
-func (err *CodeError) Error() (msg string) {
-	msg = fmt.Sprintf("%d | %d | %s", err.errorCode, err.subErrorCode, err.errorMessage)
-	return
-}
-
-func (err *CodeError) ErrorCode() (ec int) {
+func (err CarrierCodeError) ErrorCode() (ec int) {
 	ec = err.errorCode
 	return
 }
 
-func (err *CodeError) SubErrorCode() (sec int) {
+func (err CarrierCodeError) SubErrorCode() (sec int) {
 	sec = err.subErrorCode
 	return
 }
 
-func (err *CodeError) ErrorMessage() (em string) {
+func (err CarrierCodeError) ErrorMessage() (em string) {
 	em = err.errorMessage
+	return
+}
+
+func (err CarrierCodeError) Error() (msg string) {
+	msg = fmt.Sprintf("%d | %d | %s", err.errorCode, err.subErrorCode, err.errorMessage)
+	return
+}
+
+func (err CarrierCodeError) FullError() (msg string) {
+	msg = err.Error()
 	return
 }
